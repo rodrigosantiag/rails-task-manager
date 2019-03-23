9e9629b33918677573566ff3c4eba57f03989a90
Rodrigo Santiago <rodrigo@santive.com>
2019-03-23 13:01:44 -0300

    Adding update action to users controller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
-  let!(:user) { create(:user) }
-  let(:user_id) { user.id }
+  let!(:user) {create(:user)}
+  let(:user_id) {user.id}
 
-  before { host! 'api.taskmanager.local' }
+  before {host! 'api.taskmanager.local'}
 
   describe 'Get /users/:id' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
@@ -24,7 +24,7 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when user does not exist' do
-      let(:user_id) { 10000 }
+      let(:user_id) {10000}
 
       it 'return status code 404' do
         expect(response).to have_http_status(404)
@@ -34,12 +34,12 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'POST /users' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
       post '/users', params: {user: user_params}, headers: headers
     end
 
     context 'when the request params are valid' do
-      let(:user_params) { attributes_for(:user) }
+      let(:user_params) {attributes_for(:user)}
 
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
@@ -52,10 +52,10 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when the request params are invalid' do
-      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+      let(:user_params) {attributes_for(:user, email: 'invalid_email@')}
 
       it 'returns status code 422' do
-        expect(response).to have_http_status(422)        
+        expect(response).to have_http_status(422)
       end
 
       it 'returns the json data for the errors' do
@@ -64,4 +64,39 @@ RSpec.describe 'Users API', type: :request do
       end
     end
   end
+
+  describe 'PUT /users/:id' do
+    before do
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+      put "/users/#{user_id}", params: {user: user_params}, headers: headers
+    end
+
+
+    context 'when the request params are valid' do
+      let(:user_params) {{email: 'new@mail.com'}}
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the json data for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) {{email: 'invalid_email@'}}
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
+
 end
