44a220885efcbcec147482eb4357c759fdc972fc
Rodrigo Santiago <rodrigo@santive.com>
2019-03-23 15:52:16 -0300

    Adding auth token and #info to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   let(:user) { build(:user) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('rodrigosantiag@gmail.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
 
+  describe '#info' do
+    it 'returns email and created_at' do
+      user.save!
 
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
