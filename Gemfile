2bdc9bdaa5cfb3f6fdf83d041d3453043a3697c2
Rodrigo Santiago <rodrigo@santive.com>
2019-03-30 00:00:20 -0300

    Installing devise_token_auth

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'active_model_serializers', '~> 0.10.0'
 gem 'ransack'
+gem 'omniauth'
+gem 'devise_token_auth'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  include DeviseTokenAuth::Concerns::User
+
   has_many :tasks, dependent: :destroy
 
   validates_uniqueness_of :auth_token
