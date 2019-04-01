e62d0ededb22776e205586e4d8c35876787b34a2
Rodrigo Santiago <rodrigo@santive.com>
2019-03-31 22:57:45 -0300

    Adding pt-BR language

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module RailsTaskManager
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = :'pt-BR'
+
   end
 end
