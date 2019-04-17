70b13e000b12881019e03521323be3363d60cbdb
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-17 07:06:42 -0300

    Fixing namespace on serializers

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,4 +1,4 @@
-class API::V2::TaskSerializer < ActiveModel::Serializer
+class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at,
              :short_description, :is_late, :deadline_to_br
 
